ad3a4914148f585afadfa328f32a86df7ac708e0
PedroAugusto <nerdindiano@gmail.com>
2021-03-03 12:30:10 -0300

    criado migration, model de usuario, controller de usuario e rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express, { request } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+import './database/index.ts';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({menssagem: "Olá mundo - NLW04!"})
-})
-
-app.post("/", (request, response) => {
-  return response.json({message: "os dados foram salvos com sucesso!"})
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () =>console.log('Server is running caraio!'));
